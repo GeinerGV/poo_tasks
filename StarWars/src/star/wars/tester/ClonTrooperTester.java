e57ddb227058df7cb2b58bd591a9ac4a5da05e01
GeinerGV <geiner120300@gmail.com>
2019-04-04 21:15:09 -0500

    ejercicio propuesto 1

--- StarWars/src/star/wars/tester/ClonTrooperTester.java
+++ StarWars/src/star/wars/tester/ClonTrooperTester.java
@@ -4,15 +4,9 @@ import fuerzas.imperiales.ClonTrooper;
 
 public class ClonTrooperTester {
     public static void main(String[] args) {
-        ClonTrooper ct1 = new ClonTrooper();
-        ct1.setRango("SARGENTO");
-        ct1.setEspecialidad("ASALTO");
-        ct1.setArmamento("RIFLE DC-15");
+        ClonTrooper ct1 = new ClonTrooper("SARGENTO", "ASALTO", "RIFLE DC-15");
 
-        ClonTrooper ct2 = new ClonTrooper();
-        ct2.setRango("COMANDANTE");
-        ct2.setEspecialidad("ASALTO");
-        ct2.setArmamento("BLASTER DC-15");
+        ClonTrooper ct2 = new ClonTrooper("COMANDANTE", "ASALTO", "BLASTER DC-15");
 
         ct1.disparar();
         ct1.disparar();
