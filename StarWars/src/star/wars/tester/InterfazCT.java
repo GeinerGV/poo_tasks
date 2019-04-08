d775a20206feef29640a5830561dce331b774463
GeinerGV <geiner120300@gmail.com>
2019-04-07 21:53:42 -0500

    fin

--- StarWars/src/star/wars/tester/InterfazCT.java
+++ StarWars/src/star/wars/tester/InterfazCT.java
@@ -1,5 +1,9 @@
 package star.wars.tester;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
