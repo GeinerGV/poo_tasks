3a425fa08727e72d98292061a41426e3685f37b2
Geiner GV <Geiner120300@gmail.com>
2019-04-20 01:32:05 -0500

    Alumno sin getters/setters

---
+++ src/edu/usil/bean/Alumno.java
@@ -0,0 +1,24 @@
+package edu.usil.bean;
+
+import java.io.Serializable;
+
+public class Alumno implements Serializable {
+	private String codigo;
+	private String nombre;
+	private String apellido;
+	private int ciclo;
+	
+	public Alumno() {}
+	
+	public Alumno(String codigo, String nombre, String apellido, int ciclo) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.ciclo = ciclo;
+	}
+	
+	//@overwrite|override
+	public String toString() {
+		return codigo + "\t" + nombre + "\t" + apellido + "\t" + ciclo;
+	}
+}
