b32b18d8bb77f0468ad8084566f5ad1e6fba363a
Geiner GV <Geiner120300@gmail.com>
2019-04-20 01:45:54 -0500

    Test01 - parte B

---
+++ src/edu/usil/test/Test01.java
@@ -0,0 +1,45 @@
+package edu.usil.test;
+
+import java.util.Scanner;
+
+import edu.usil.bean.Alumno;
+
+public class Test01 {
+
+	Alumno[] alumnos;
+	
+	public static void main(String[] args) {
+		Test01 t01 = new Test01();
+		t01.ejemplo1();
+		t01.listar();
+	}
+	
+	public void ejemplo1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cuantos alumnos vas a ingresar ?");
+		int n = sc.nextInt();
+		sc.nextLine();
+		
+		alumnos = new Alumno[n];
+		
+		for (int i=0; i<n; i++) {
+			System.out.println("Ingrese el código");
+			String codigo = sc.nextLine();
+			System.out.println("Ingrese el nombre");
+			String nombre = sc.nextLine();
+			System.out.println("Ingrese el apellido");
+			String apellido = sc.nextLine();
+			System.out.println("Ingrese el ciclo");
+			int ciclo = sc.nextInt();
+			
+			Alumno a = new Alumno(codigo, nombre, apellido, ciclo);
+			alumnos[i] = a;
+		}
+	}
+	
+	public void listar() {
+		for (int i=0; i<n; i++) {
+			System.out.println(alumnos[i]);
+		}
+	}
+}
