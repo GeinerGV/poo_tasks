c004bb3d2c3c22771ed872422c13f3bae4a39e38
Geiner GV <Geiner120300@gmail.com>
2019-04-20 00:44:08 -0500

    Finish

--- src/com/ejemplo/arreglos/TestArray01.java
+++ src/com/ejemplo/arreglos/TestArray01.java
@@ -2,6 +2,30 @@ package com.ejemplo.arreglos;
 
 public class TestArray01 {
 	public static void main(String[] args) {
+		int[] array1 = {1,3,5,7,9,11,13,15};
+		int[] array2;
 		
+		System.out.println("Array1 :");
+		for (int i=0; i<array1.length; i++) {
+			System.out.print("["+i+"]="}array1[i]+" , ");
+		}
+		System.out.println("\n");
+		
+		array2 = array1;
+		for (int i=0; i<array2.length; i+=2) {
+			array2[i]=i;
+		}
+		
+		System.out.println("Array1 :");
+		for (int i=0; i<array1.length; i++) {
+			System.out.print("["+i+"]="}array1[i]+" , ");
+		}
+		System.out.println("\n");
+		
+		System.out.println("Array2 :");
+		for (int i=0; i<array2.length; i++) {
+			System.out.print("["+i+"]="}array2[i]+" , ");
+		}
+		System.out.println("\n");
 	}
 }
