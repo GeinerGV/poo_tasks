d76e9c96894a16019d58160e00184149c64bb928
GeinerGV <geiner120300@gmail.com>
2019-04-21 16:08:57 -0500

    w/ getters/setters

--- src/edu/usil/bean/Alumno.java
+++ src/edu/usil/bean/Alumno.java
@@ -22,4 +22,60 @@ public class Alumno implements Serializable {
 	public String toString() {
 		return "Código: "+codigo + "\n" + "Nombre: "+nombre + "\n" +"Apellidos: "+ apellido + "\n" + "Ciclo: "+ ciclo;
 	}
+
+	/**
+	 * @return the codigo
+	 */
+	public String getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	/**
+	 * @return the ciclo
+	 */
+	public int getCiclo() {
+		return ciclo;
+	}
+
+	/**
+	 * @param ciclo the ciclo to set
+	 */
+	public void setCiclo(int ciclo) {
+		this.ciclo = ciclo;
+	}
 }
