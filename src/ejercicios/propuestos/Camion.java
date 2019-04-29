fa92acc7e474718519e9b38829392fcd31ea8499
GeinerGV <geiner120300@gmail.com>
2019-04-28 22:03:33 -0500

    spaces to tabs

--- src/ejercicios/propuestos/Camion.java
+++ src/ejercicios/propuestos/Camion.java
@@ -1,16 +1,16 @@
 package ejercicios.propuestos;
 
 public class Camion extends Vehiculo {
-    private int numeroRuedas;
-    private int pesoMaximo;
+	private int numeroRuedas;
+	private int pesoMaximo;
 
-    public Camion(String marca, String color, int numeroRuedas, int pesoMaximo) {
-        super(marca, color);
-        this.numeroRuedas = numeroRuedas;
-        this.pesoMaximo = pesoMaximo;
-    }
+	public Camion(String marca, String color, int numeroRuedas, int pesoMaximo) {
+		super(marca, color);
+		this.numeroRuedas = numeroRuedas;
+		this.pesoMaximo = pesoMaximo;
+	}
 
-    public String toString() {
-        return "Camion("+marca+", "+color+", ruedas: "+numeroRuedas+", pesoMax: "+pesoMaximo+")";
-    }
+	public String toString() {
+		return "Camion("+marca+", "+color+", ruedas: "+numeroRuedas+", pesoMax: "+pesoMaximo+")";
+	}
 }
--- src/ejercicios/propuestos/Vehiculo.java
+++ src/ejercicios/propuestos/Vehiculo.java
@@ -1,11 +1,11 @@
 package ejercicios.propuestos;
 
 public class Vehiculo {
-    private String marca;
-    private String color;
+	private String marca;
+	private String color;
 
-    public Vehiculo(String marca, String color) {
-        this.marca = marca;
-        this.color = color;
-    }
+	public Vehiculo(String marca, String color) {
+		this.marca = marca;
+		this.color = color;
+	}
 }
