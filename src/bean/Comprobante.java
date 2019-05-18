6636a678555a93e2598b8e2af494a7a310d5bcc5
Geiner GV <Geiner120300@gmail.com>
2019-05-17 22:02:41 -0500

    add correlativo attribute

--- src/bean/Comprobante.java
+++ src/bean/Comprobante.java
@@ -3,11 +3,13 @@ package bean;
 import java.io.Serializable;
 
 public class Comprobante implements Serializable {
+	private int correlativo;
 	private String fecha;
 	private String ruc_emisor;
 	private String razon_emisor;
 	public Comprobante() {}
-	public Comprobante(String fch, String ruc, String razon) {
+	public Comprobante(int correlativo, String fch, String ruc, String razon) {
+		this.correlativo = correlativo;
 		fecha = fch;
 		ruc_emisor = ruc;
 		razon_emisor = razon;
