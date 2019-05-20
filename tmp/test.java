a15d41f682cbd7a84400fe22342c881e76366ad0
GeinerGV <geiner120300@gmail.com>
2019-05-19 23:21:47 -0500

    otro tester

---
+++ tmp/test.java
@@ -0,0 +1,13 @@
+/**
+ * test
+ */
+public class test {
+
+	public static void main(String[] args) {
+		double val = Math.random();
+		System.out.println(val);
+		System.out.println((int)val);
+		System.out.println((int)Math.round(val));
+		System.out.println(val>0.5);
+	}
+}
