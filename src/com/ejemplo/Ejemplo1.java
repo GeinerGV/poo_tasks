98eb0682071cd5384224f5e5ddbea769c0e44260
Geiner GV <Geiner120300@gmail.com>
2019-05-24 17:14:42 -0500

    Update Ejemplo1.java

--- src/com/ejemplo/Ejemplo1.java
+++ src/com/ejemplo/Ejemplo1.java
@@ -2,13 +2,18 @@ package com.ejemplo;
 
 public class Ejemplo1 {
 	public staic void main() {
-		int j = 0;
+		int j;
 		try {
 			j = 1/j;
 			System.out.pritln("Pasamos por la división");
 			
-		} catch(ArithmeticException e) {
+		} catch(NullPointerException e) {
 			System.out.pritln("Capturamos un error en la división");
+		} catch(Exception e) {
+			System.out.println("Se produjo el siguiente error:");
+			e.printStackTrace();
+		} finally {
+			System.out.pritln("RetoCumplido¡");
 		}
 		System.out.pritln("Seguimos con el programa");
 	}
